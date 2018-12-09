01645174065fa586978eb96faab6a24bee7007e2
Sir Alexsearch <aleksander.repnikov@gmail.com>
2018-12-09 19:01:19 +0100

    Retrofit + GSON

--- app/src/main/java/com/daftmobile/a4bhomework5/PokemonAPI.kt
+++ app/src/main/java/com/daftmobile/a4bhomework5/PokemonAPI.kt
@@ -8,6 +8,6 @@ import retrofit2.http.Path
 interface PokemonAPI {
     // /api/pokemon/:number/peek
     @GET("/api/pokemon/{pokedexIndex}/peek")
-    fun peekPokemonInfo(@Path("pokedexIndex") pokedexIndex: String): Call<ResponseBody>
+    fun peekPokemonInfo(@Path("pokedexIndex") pokedexIndex: String): Call<PokemonItem>
 
 }
--- app/src/main/java/com/daftmobile/a4bhomework5/PokemonFetcher.kt
+++ app/src/main/java/com/daftmobile/a4bhomework5/PokemonFetcher.kt
@@ -1,31 +1,40 @@
 package com.daftmobile.a4bhomework5
 
 import android.provider.Settings
+import okhttp3.OkHttpClient
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class PokemonFetcher: PokemonDataSource {
 
-    private val retrofit = Retrofit.Builder()
-            .baseUrl("https://switter.app.daftmobile.com/")
+    private val client = OkHttpClient.Builder()
             .build()
+
+    private val retrofit = Retrofit.Builder()
+            .client(client)
+            .baseUrl("https://switter.app.daftmobile.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    // trzeba stworzyć drugi plik JokeApi, taki, w którym metody zwracają Call<Joke>
 
     private val pokemonApi = retrofit.create(PokemonAPI::class.java)
 
     override fun fetch(pokedexIndex: String, onSuccess: (String) -> Unit, onError: (String) -> Unit) {
         val call = pokemonApi.peekPokemonInfo(pokedexIndex)
 
-        call.enqueue(object : Callback<ResponseBody> {
-            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+        call.enqueue(object : Callback<PokemonItem> {
+            override fun onFailure(call: Call<PokemonItem>, t: Throwable) {
                 onError(t.message ?: "No message")
             }
 
-            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+            override fun onResponse(call: Call<PokemonItem>, response: Response<PokemonItem>) {
                 if (response.isSuccessful) {
-                    onSuccess(response.body()?.string() ?: "Weird empty response")
+                    onSuccess(response.body()?.name ?: "Weird empty response")
                 } else {
                     onError("Serwer zwrocil: ${response.code()}")
                 }
